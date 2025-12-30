3cb9e47da96df30164fcbf3bdfab4321d838fc9e
davirezo <fksdavi@gmail.com>
2025-12-30 11:19:50 -0300

    Obtendo um usuario por Id

--- src/main/java/com/educandoweb/workshopmongo/service/UserService.java
+++ src/main/java/com/educandoweb/workshopmongo/service/UserService.java
@@ -2,10 +2,12 @@ package com.educandoweb.workshopmongo.service;
 
 import com.educandoweb.workshopmongo.domain.User;
 import com.educandoweb.workshopmongo.repository.UserRepository;
+import com.educandoweb.workshopmongo.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +18,14 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+
+    public User findById(String id){
+        Optional<User> user = repo.findById(id);
+
+        return user.orElseThrow(() ->
+                new ObjectNotFoundException("Objeto não encontrado")
+        );
     }
 }
